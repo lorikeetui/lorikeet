99e111353c75e002510a7c9506457015f672fe8a
Pierre Bertet <hello@pierre.world>
2019-03-11 12:12:49 +0000

    Fix react-spring imports

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Spring, animated } from 'react-spring'
+import { Spring, animated } from 'react-spring/renderprops'
 import { noop } from '../../utils'
 import { springs, unselectable } from '../../styles'
 
